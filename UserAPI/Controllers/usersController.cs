21ffae2ed1747def906b25344de5c758824aadb6
Billy <bimganda@gmail.com>
2023-04-03 16:35:52 -0400

    create user controller

---
+++ UserAPI/Controllers/usersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserAPI.Services;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public usersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
