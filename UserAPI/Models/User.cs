e7217fda9839d4594d90610cafaf30dd692bd50d
Billy <bimganda@gmail.com>
2023-04-03 16:21:53 -0400

    add user class

---
+++ UserAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAPI.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
