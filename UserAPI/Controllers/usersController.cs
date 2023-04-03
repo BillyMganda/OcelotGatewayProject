22f6973c616f86cbbab2a0f225e88382f179594e
Billy <bimganda@gmail.com>
2023-04-03 16:37:32 -0400

    implement all CRUD methods in controller

--- UserAPI/Controllers/usersController.cs
+++ UserAPI/Controllers/usersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UserAPI.Models;
 using UserAPI.Services;
 
 namespace UserAPI.Controllers
@@ -12,6 +13,33 @@ namespace UserAPI.Controllers
         public usersController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> UserList()
+        {
+            var userList = _userService.GetUserList();
+            return userList;
+        }
+        [HttpGet("{id}")]
+        public User GetUserById(Guid Id)
+        {
+            return _userService.GetUserById(Id);
+        }
+        [HttpPost]
+        public User AddUser(User user)
+        {
+            return _userService.AddUser(user);
+        }
+        [HttpPut]
+        public User UpdateUser(User user)
+        {
+            return _userService.UpdateUser(user);
+        }
+        [HttpDelete("{id}")]
+        public bool DeleteUser(Guid id)
+        {
+            return _userService.DeleteUser(id);
         }
     }
 }
