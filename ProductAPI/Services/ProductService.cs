ceddb76b30df008c522cb2ed082f1df6fb41efeb
Billy <bimganda@gmail.com>
2023-04-03 15:54:41 -0400

    create an implementation class of the product service interface

---
+++ ProductAPI/Services/ProductService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data;
+using ProductAPI.Models;
+
+namespace ProductAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ProductDBContext _dbContext;
+        public ProductService(ProductDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        public Product AddProduct(Product product)
+        {
+            var result = _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+
+        public bool DeleteProduct(Guid Id)
+        {
+            var filteredData = _dbContext.Products.Where(x => x.ProductId == Id).FirstOrDefault();
+            if(filteredData != null)
+            {
+                _dbContext.Remove(filteredData);
+                _dbContext.SaveChanges();
+                return true;
+            }               
+            return false;
+        }
+
+        public Product GetProductById(Guid id)
+        {
+            return _dbContext.Products.Where(x => x.ProductId == id).FirstOrDefault()!;
+        }
+
+        public IEnumerable<Product> GetProductList()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            var result = _dbContext.Products.Update(product);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+    }
+}
